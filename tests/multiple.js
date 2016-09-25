a0450d41195dc2526308f3adde510d2967efcc0f
Sam Richard <sam@snug.ug>
2016-09-25 17:09:46 -0400

    :shirt: Fix linting errors

--- tests/multiple.js
+++ tests/multiple.js
@@ -1,7 +1,6 @@
 import test from 'ava';
 import build from '../lib/build';
 import endpoints from '../lib/endpoints';
-import curry from '../lib/curry';
 
 test('Multiple Resolves', t => {
   process.env.PUNCHCARD = 'https://punchcard.io';
@@ -61,43 +60,3 @@ test('Chaining One', t => {
       t.fail();
     });
 });
-
-// test('Chaining Multiple', t => {
-//   const input = [
-//     {
-//       get: 'type',
-//       type: 'foo-bar',
-//       punchcard: 'https://punchcard.com',
-//       dir: 'asc',
-//     },
-//     {
-//       get: 'one',
-//       type: 'waldo',
-//       id: '2442-1337-4224',
-//       punchcard: 'https://punchcard.com',
-//       page: 3,
-//       limit: 15,
-//     },
-//   ];
-
-//   const expected = [
-//     {
-//       method: 'GET',
-//       url: 'https://punchcard.com/api/types/foo-bar?sort_dir=asc',
-//     },
-//     {
-//       method: 'GET',
-//       url: 'https://punchcard.com/api/types/waldo/2442-1337-4224?page=3&per_page=15',
-//     },
-//   ];
-
-//   const go = input.map(curry);
-
-//   return Promise.all(go)
-//     .then(result => {
-//       t.deepEqual(result, expected);
-//     }).catch(e => {
-//       console.log(e.message); // eslint-disable-line no-console
-//       t.fail();
-//     });
-// });
