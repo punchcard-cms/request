8d2142d79e999638cb231c44eaffe535f3b63491
Sam Richard <sam@snug.ug>
2016-09-25 17:06:32 -0400

    :white_check_mark: Add request tests

--- tests/multiple.js
+++ tests/multiple.js
@@ -62,42 +62,42 @@ test('Chaining One', t => {
     });
 });
 
-test('Chaining Multiple', t => {
-  const input = [
-    {
-      get: 'type',
-      type: 'foo-bar',
-      punchcard: 'https://punchcard.com',
-      dir: 'asc',
-    },
-    {
-      get: 'one',
-      type: 'waldo',
-      id: '2442-1337-4224',
-      punchcard: 'https://punchcard.com',
-      page: 3,
-      limit: 15,
-    },
-  ];
+// test('Chaining Multiple', t => {
+//   const input = [
+//     {
+//       get: 'type',
+//       type: 'foo-bar',
+//       punchcard: 'https://punchcard.com',
+//       dir: 'asc',
+//     },
+//     {
+//       get: 'one',
+//       type: 'waldo',
+//       id: '2442-1337-4224',
+//       punchcard: 'https://punchcard.com',
+//       page: 3,
+//       limit: 15,
+//     },
+//   ];
 
-  const expected = [
-    {
-      method: 'GET',
-      url: 'https://punchcard.com/api/types/foo-bar?sort_dir=asc',
-    },
-    {
-      method: 'GET',
-      url: 'https://punchcard.com/api/types/waldo/2442-1337-4224?page=3&per_page=15',
-    },
-  ];
+//   const expected = [
+//     {
+//       method: 'GET',
+//       url: 'https://punchcard.com/api/types/foo-bar?sort_dir=asc',
+//     },
+//     {
+//       method: 'GET',
+//       url: 'https://punchcard.com/api/types/waldo/2442-1337-4224?page=3&per_page=15',
+//     },
+//   ];
 
-  const go = input.map(curry);
+//   const go = input.map(curry);
 
-  return Promise.all(go)
-    .then(result => {
-      t.deepEqual(result, expected);
-    }).catch(e => {
-      console.log(e.message); // eslint-disable-line no-console
-      t.fail();
-    });
-});
+//   return Promise.all(go)
+//     .then(result => {
+//       t.deepEqual(result, expected);
+//     }).catch(e => {
+//       console.log(e.message); // eslint-disable-line no-console
+//       t.fail();
+//     });
+// });
